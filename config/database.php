7c8e0d4c485aca2a3bfbcb3f5ce6f2c21eb8b0ff
Sundaresan SIVAGNANAMOORTHY <susivagn@e1r5p18.42.fr>
2018-09-25 20:21:41 +0200

    env file and db setting updated

--- config/database.php
+++ config/database.php
@@ -3,3 +3,32 @@
 <html>
     <h1>Database Page</h1>
 </html>
+
+Data for camagru
+
+    account :
+        - user_id
+        - user_name
+        - user_surname
+        - user_email
+        - user_password
+        - user_timestamp
+        - access_lvl -> public -> client -> moderator -> admin
+        - user_bio ?
+        - id of all post
+        - id of all comment
+
+    imgpost :
+        - id
+        - id of the owner of the image
+        - number of likes
+        - number of comment
+        - timestamp
+    
+    compost :
+        - id
+        - id of the commented image
+        - id of the user who commented
+        - number of likes ?
+        - lenght of the post
+        - timestamp
---
+++ resources/env.php
@@ -0,0 +1,21 @@
+<?php
+    const DB_HOST = 'localhost';
+	const DB_USER = 'root';
+	const DB_PASSWD = 'secret';
+	const DB_DATABASE = 'website';
+
+	const ACCOUNTS_MAX_SIZE_NAME = 20;
+	const ACCOUNTS_MAX_SIZE_SURNAME = 20;
+	const ACCOUNTS_MAX_SIZE_EMAIL = 50;
+	const ACCOUNTS_MAX_SIZE_PASSWD = 64;
+	const ACCOUNTS_HASH_ALGO = 'whirlpool';
+    const ACCOUNTS_HASH_SALT = 'CTnbwBx8fI';
+    
+    const COMMENTS_MAX_SIZE = 500;
+
+    const ACCESS_LVL_PUBLIC = 1;
+    const ACCESS_LVL_CLIENT = 2;
+    const ACCESS_LVL_MODO = 3;
+    const ACCESS_LVL_ADMIN = 31;
+
+?>
