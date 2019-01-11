0e20807a2c52028d218eab9ad575e7feddc6e602
Nwtype <newcratie@gmail.com>
2019-01-11 19:01:51 +0700

    Login working

--- v1/controller/register_form.php
+++ v1/controller/register_form.php
@@ -1,48 +1,5 @@
 <?php
 
+require(__ROOT__ . 'view/register_form.php');
 
-/*
-*/
 ?>
-<link rel="stylesheet" href="/v1/css/signpage.css"/>
-
-
-<div class="register-page">
-    <div class="form">
-        <form id="register-form" class="register-form">
-            <input type="text" placeholder="login" name="login"/>
-            <input type="password" placeholder="password" name="password"/>
-            <input type="text" placeholder="email address" name="email"/>
-            <button type="submit">create</button>
-            <p class="message">Already registered? <a href="?page=login_form">Sign In</a></p>
-        </form>
-    </div>
-</div>
-
-<script>
-    let form = document.getElementById('register-form');
-    let XHR = new XMLHttpRequest();
-
-    form.addEventListener('submit', function (e){
-
-        e.preventDefault();
-        let formData = new FormData(form);
-
-        XHR.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                    console.log(this.responseText);
-            }
-        };
-
-        XHR.addEventListener('load', function (event) {
-        });
-
-        XHR.addEventListener('error', function (event) {
-            alert('Something goes wrong');
-        });
-
-        XHR.open('POST', 'v1/controller/register.php');
-
-        XHR.send(formData);
-    }, false);
-</script>
--- v1/view/register_form.php
+++ v1/view/register_form.php
@@ -1,1 +1,18 @@
 <?php
+?>
+<link rel="stylesheet" href="/v1/css/signpage.css"/>
+
+
+<div class="register-page">
+    <div class="form">
+        <form id="register-form" class="register-form">
+            <input type="text" placeholder="login" name="login"/>
+            <input type="password" placeholder="password" name="password"/>
+            <input type="text" placeholder="email address" name="email"/>
+            <button type="submit">create</button>
+            <p class="message">Already registered? <a href="?page=login_form">Sign In</a></p>
+        </form>
+    </div>
+</div>
+<script src="/v1/js/register.js"></script>
+
