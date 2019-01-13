c41597206140aaefe350360ad92f4c0a9ec43346
Nwtype <newcratie@gmail.com>
2019-01-13 18:35:32 +0700

    Install sh update

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-docker exec test-cama_web_1 mysql -uroot -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'secret'"
-docker exec test-cama_web_1 mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';"
-docker exec test-cama_web_1 mysql -uroot -e "create database camagru;"
+docker exec camagru_web_1 mysql -uroot -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'secret'"
+docker exec camagru_web_1 mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';"
+docker exec camagru_web_1 mysql -uroot -e "create database camagru;"
