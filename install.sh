485362dc97549b0cddb006e2fdf8131088ef3e7d
Nwtype <newcratie@gmail.com>
2019-01-14 18:15:23 +0700

    Gallery is fonctionnal ar 50%

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 docker exec camagru_web_1 mysql -uroot -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'secret'"
 docker exec camagru_web_1 mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';"
 docker exec camagru_web_1 mysql -uroot -e "create database camagru;"
+mkdir img
