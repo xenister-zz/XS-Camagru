49459f024f0f21d761186087b672c67ae8dcdf10
Nwtype <newcratie@gmail.com>
2019-02-21 14:08:13 +0100

    Fixe header

--- view/register_form.php
+++ view/register_form.php
@@ -1,34 +1,10 @@
 <?php
 ?>
-<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"><link rel="stylesheet" href="/css/signpage.css"/>-->
 <link rel="stylesheet" href="/css/signpage.css"/>
 <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 
-<!--<div class="register-page">-->
-<!--    <div class="form">-->
-<!--        <h1 class="titleregister"> Register </h1><br/>-->
-<!--        <p class="headmessage"> All fields are mandatory</p><br/>-->
-<!--        --><?php //if (isset($_GET['msg'])) {
-//            echo "<div id = \"form-error\" class=\"alert alert-danger\" role = \"alert\" >";
-//            echo $_GET['msg'];
-//            echo "</div >";
-//        }; ?>
-<!--        <form method="post" id="register-form" class="register-form">-->
-<!--            <input type="text" placeholder="Login" name="userlogin" required/>-->
-<!--            <input type="password" placeholder="Password" name="password" required/>-->
-<!--            <input type="password" placeholder="Confirm password" name="confirm_password" required/>-->
-<!--            <input type="text" placeholder="Email address" name="usermail" required/>-->
-<!---->
-<!--            <button id="submit_register" type="submit" name="submit">create</button>-->
-<!--            <p class="message">Already registered? <a href="?page=landing">Sign In</a></p>-->
-<!--        </form>-->
-<!--    </div>-->
-<!--</div>-->
-
 <div class="card-content form">
-
     <h1 class="titlelogin"> Register </h1>
-
     <form id="login-form" class="login-form" method="post">
         <div class="field">
             <label class="label">Username</label>
@@ -82,5 +58,5 @@
     </form>
 
 </div>
-<script src="/js/register.js"></script>
+<!--<script src="/js/register.js"></script>-->
 
